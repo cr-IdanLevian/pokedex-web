07dc7b2b40c1391424ddd035f7e92f80abb571f2
idan.levian <idan.levian@cybereason.com>
2025-04-10 14:58:37 +0300

    add provider and controller to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,8 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { RedisCacheModule } from './cache/redis-cache.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 const __DEV__ = process.env.NODE_ENV === 'development';
 
@@ -37,5 +39,7 @@ const __DEV__ = process.env.NODE_ENV === 'development';
     UserModule,
     PokemonModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
